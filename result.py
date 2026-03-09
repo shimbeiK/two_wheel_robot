7e2133df5a4268e70a975720fbf0fdd09dc9d8f0
shimbeiK <datasaver0530[gmail.com>
2026-03-09 14:09:17 +0900

    update

--- result.py
+++ result.py
@@ -3,11 +3,15 @@ import mujoco.viewer
 import time
 import numpy as np
 from stable_baselines3 import PPO
-from bike_env_v3_NonSteer import StandingEnv
+from gymnasium import wrappers
+# from bike_env_v3 import StandingEnv
+# from bike_env_v3_NonSteer_withrealnoise import StandingEnv
+from bike_env_v3_straight import StandingEnv
 
 # --- 1. Setup the Environment for Evaluation ---
 # We use ONE environment with render_mode="human"
-MODEL_PATH = "mjcf2/scene.xml"
+MODEL_PATH = "mjcf_symmetry/HBP_V2_mjcf/scene.xml"
+# MODEL_PATH = "mjcf2/scene.xml"
 m = mujoco.MjModel.from_xml_path(MODEL_PATH)
 d = mujoco.MjData(m)
 
@@ -20,18 +24,22 @@ env = StandingEnv(xml_path=MODEL_PATH, render_mode="human")
 # env.norm_reward = False
 l_wheel_id = mujoco.mj_name2id(env.model, mujoco.mjtObj.mjOBJ_JOINT, "tire_back_pitch")
 # prev_angular_vel = env.data.qvel[env.model.jnt_dofadr[l_wheel_id]]
-angular_vel = env.data.sensor("imu_gyro").data.copy()[0]+np.random.normal(0, 0.01)  # ジャイロのx軸の角速度にノイズを加える
+# angular_vel = env.data.sensor("imu_gyro").data.copy()[0]+np.random.normal(0, 0.01)  # ジャイロのx軸の角速度にノイズを加える
 
 # --- 2. Load the Trained Model ---
 # Load the zip file you just saved
 # model = PPO.load("two_wheel_robot/results/stop_withCon/20260214-1804_continue", env=env)
 model = PPO.load("two_wheel_robot/results/stop_withCon_v3/best_model", env=env)
+# model = PPO.load("two_wheel_robot/results/stop_withCon_v3/kourin21_con", env=env)
 # model = PPO.load("two_wheel_robot/results/stop_withCon/kourin25/20260222-1110_con.zip", env=env)
 # model = PPO.load("two_wheel_robot/results/stop_ignoreCon/ppo_standing_20260214-1623.zip", env=env)
 # --- 3. Run the Simulation Loop ---
 obs, _ = env.reset()
 print("Running trained model... Press Ctrl+C to stop.")
+print(env.TARGET_xVEL)
 counter = 0
+prev_odometry = 0
+action_num = 0
 while True:
     counter+=1
     # print(counter)
@@ -43,21 +51,29 @@ while True:
     obs, reward, terminated, truncated, info = env.step(action)
     # print("action:", action)
     # print("pos", obs[7], obs[8])
-    # print("action:", np.rad2deg(action[0]), action[1])
+    action_num += np.rad2deg(action[0]*np.deg2rad(80))
+    # print("action:", np.rad2deg(action[0]*np.deg2rad(80)), action_num, action[1])
     # print("imu", np.rad2deg(obs[0]))
-    print(obs[0], action, reward)
+    # print(obs[2], obs[3], counter)
+    print(env.TARGET_xVEL, obs[2]*0.031, env.total_Xpos, env.total_Ypos, counter)
+    
+    prev_odometry = env.total_odometry
     # print("total_odometory:", env.total_odometory)
     # print("angular_vel:", prev_angular_vel)
     # print(obs[0], obs[1], obs[3], obs[4], obs[5])
 
     # Render is often handled automatically by render_mode="human" in gymnasium,
     # but we call it here just in case your custom env requires it.
+    # time.sleep(0.01)
     env.render()
     
     # Slow down slightly to match real-time (optional, depends on your PC speed)
-    time.sleep(0.01)
+    time.sleep(0.02)
 
     if terminated or truncated:
         print("ouch!")
         obs, _ = env.reset()
+        print(env.TARGET_xVEL)
+        time.sleep(2)
         counter = 0
+        action_num = 0
